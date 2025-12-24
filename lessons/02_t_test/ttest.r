1b2cc42d3eaaf2c28349f69bb2ff7f9973254cba
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2025-12-24 16:50:31 +0500

    chore: rename folder name to 02_t_test

---
+++ lessons/02_t_test/ttest.r
@@ -0,0 +1,17 @@
+# One-Sample t-Test
+
+data <- c(1, 2, 3, 4, 5)
+t.test(data, mu = 3)
+
+# data <- c(5, 6, 7, 8, 5, 6, 7)
+# t.test(data, mu = 8)
+# mean(data)
+#
+# group1 <- c(5, 6, 7, 5, 6)
+# group2 <- c(7, 8, 9, 6, 7)
+#
+# t.test(group1, group2)
+#
+# before <- c(100, 102, 98, 105)
+# after <- c(102, 100, 99, 107)
+# t.test(before, after, paired = TRUE)
